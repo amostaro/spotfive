619bd5413b7bf203e1ab4f564071f65334f6b73f
mmodanez <mmodanez@ciandt.com>
2022-08-23 11:43:07 -0300

    BOOT-893 Create Entities

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/domain/data/entity/ArtistEntity.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/domain/data/entity/ArtistEntity.java
@@ -1,4 +1,30 @@
 package com.ciandt.summit.bootcamp2022.domain.data.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Artistas")
 public class ArtistEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "Id", nullable = false)
+    private String id;
+
+    @Column(name = "Nome")
+    private String name;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "artistEntity",fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private MusicEntity musicEntity;
 }
--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/domain/data/entity/MusicEntity.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/domain/data/entity/MusicEntity.java
@@ -1,4 +1,33 @@
 package com.ciandt.summit.bootcamp2022.domain.data.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Musicas")
 public class MusicEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "Id", nullable = false)
+    private String id;
+
+    @Column(name = "Nome")
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ArtistaId", referencedColumnName = "Id")
+    private ArtistEntity artistEntity;
+
+    @Column(name = "Id", insertable = false, updatable = false)
+    private String artistId;
+
 }
