24754469ecd695de5bf5f2c427a55e7e636fb916
Anna Ripp <annaripp@ciandt.com>
2022-08-24 13:58:29 -0300

    Length Validation and global exception implementations

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/application/controller/MusicController.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/application/controller/MusicController.java
@@ -3,6 +3,7 @@ package com.ciandt.summit.bootcamp2022.application.controller;
 import com.ciandt.summit.bootcamp2022.domain.data.dto.DataDTO;
 import com.ciandt.summit.bootcamp2022.domain.data.dto.MusicDTO;
 import com.ciandt.summit.bootcamp2022.domain.port.interfaces.MusicServicePort;
+import com.ciandt.summit.bootcamp2022.domain.service.exception.LengthValidationException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @RestController
@@ -22,7 +21,7 @@ public class MusicController {
     private final MusicServicePort musicServicePort;
 
     @GetMapping
-    public DataDTO get(@RequestParam String filtro) {
+    public DataDTO get(@RequestParam String filtro) throws LengthValidationException {
         List<MusicDTO> allByNameLikeIgnoreCase = musicServicePort.findAllByNameLikeIgnoreCase(filtro);
         DataDTO dataDTO = new DataDTO();
         dataDTO.setData(new HashSet<>(allByNameLikeIgnoreCase));
--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/domain/service/MusicServiceImpl.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/domain/service/MusicServiceImpl.java
@@ -3,6 +3,7 @@ package com.ciandt.summit.bootcamp2022.domain.service;
 import com.ciandt.summit.bootcamp2022.domain.data.dto.MusicDTO;
 import com.ciandt.summit.bootcamp2022.domain.port.interfaces.MusicServicePort;
 import com.ciandt.summit.bootcamp2022.domain.port.repository.MusicRepositoryPort;
+import com.ciandt.summit.bootcamp2022.domain.service.exception.LengthValidationException;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
@@ -14,7 +15,11 @@ public class MusicServiceImpl implements MusicServicePort {
 
 
     @Override
-    public List<MusicDTO> findAllByNameLikeIgnoreCase(String searchName) {
-        return this.musicRepositoryPort.findArtistEntityAndMusicEntityListOrderByName(searchName);
+    public List<MusicDTO> findAllByNameLikeIgnoreCase(String searchName) throws LengthValidationException {
+        if(searchName.length() < 3) {
+            throw new LengthValidationException("Operação inválida. A busca precisa ter no mínimo 3 caracteres.");
+        }
+            return this.musicRepositoryPort.findArtistEntityAndMusicEntityListOrderByName(searchName);
+
     }
 }
