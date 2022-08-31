88aaa216edf4926d75ac1bf0428f7c12a2b872cc
amostaro <amostaro@ciandt.com>
2022-08-31 08:35:30 -0300

    BOOT-1073 - PlaylistEntity

---
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/domain/data/entity/PlaylistEntity.java
@@ -0,0 +1,34 @@
+package com.ciandt.summit.bootcamp2022.domain.data.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity(name = "Playlists")
+@Table(name = "Playlists")
+public class PlaylistEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "Id", nullable = false)
+    private String id;
+
+    @JsonIgnore
+    @OneToMany(fetch = FetchType.LAZY,mappedBy = "playlistEntity", cascade = CascadeType.ALL)
+    private Set<UserEntity> userEntityList;
+
+    @ManyToMany
+    @JsonProperty(value = "playlistMusic")
+    @JoinTable(name = "PlaylistMusicas",
+    joinColumns = @JoinColumn(name = "PlaylistId", referencedColumnName = "id"),
+    inverseJoinColumns = @JoinColumn(name = "MusicaId", referencedColumnName = "id"))
+    @JsonBackReference
+    private Set<MusicEntity> musicEntityList;
+}
