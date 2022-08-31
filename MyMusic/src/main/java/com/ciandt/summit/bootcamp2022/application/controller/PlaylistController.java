42fe706da8484479c190537a7431ea4c7f79e38c
mmodanez <mmodanez@ciandt.com>
2022-08-31 11:38:22 -0300

    BOOT-1070 PlaylistController Implementation

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/application/controller/PlaylistController.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/application/controller/PlaylistController.java
@@ -1,11 +1,26 @@
 package com.ciandt.summit.bootcamp2022.application.controller;
 
+import com.ciandt.summit.bootcamp2022.domain.port.interfaces.PlaylistServicePort;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/playlists/{playlistId/musicas") //verificar URL
+@RequestMapping("/api/playlists")
 public class PlaylistController {
+    private final PlaylistServicePort playlistServicePort;
+
+    @PutMapping("/{idPlaylist}/musicas")
+    public ResponseEntity<String> updatePlaylist(@RequestParam String idMusica, @PathVariable String idPlaylist) throws PlaylistNotFoundException, MusicNotFoundException {
+
+        String updated = playlistServicePort.saveMusicInPlaylist(idMusica, idPlaylist);
+
+        return new ResponseEntity<>(updated, HttpStatus.OK);
+    }
+
 }
