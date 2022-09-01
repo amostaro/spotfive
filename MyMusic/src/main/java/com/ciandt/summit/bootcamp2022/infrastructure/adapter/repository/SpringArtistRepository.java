a93b9fdd1ab27273d7bc374df7108d460d11f45b
Anna Ripp <annaripp@ciandt.com>
2022-09-01 16:41:06 -0300

    BOOT-1067

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/infrastructure/adapter/repository/SpringArtistRepository.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/infrastructure/adapter/repository/SpringArtistRepository.java
@@ -2,16 +2,10 @@ package com.ciandt.summit.bootcamp2022.infrastructure.adapter.repository;
 
 import com.ciandt.summit.bootcamp2022.domain.data.entity.ArtistEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface SpringArtistRepository extends JpaRepository<ArtistEntity, String> {
-
-    @Query("SELECT a FROM Artistas a WHERE upper(a.name) LIKE %:name%")
-    List<ArtistEntity> findAllByNameLikeIgnoreCase(String name);
 
 }
 
--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/infrastructure/configuration/security/dto/CreateAuthorizerRequest.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/infrastructure/configuration/security/dto/CreateAuthorizerRequest.java
@@ -1,8 +1,10 @@
 package com.ciandt.summit.bootcamp2022.infrastructure.configuration.security.dto;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Setter
+@Getter
 public class CreateAuthorizerRequest {
 
     private CreateAuthorizerRequestData data;
