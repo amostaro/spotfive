45fcf94839c2b25d4351d912fe2d7158a2fee5bd
mmodanez <mmodanez@ciandt.com>
2022-09-12 10:54:15 -0300

    Basic UserService Implementation

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/domain/port/repository/UserRepositoryPort.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/domain/port/repository/UserRepositoryPort.java
@@ -1,4 +1,12 @@
 package com.ciandt.summit.bootcamp2022.domain.port.repository;
 
+import com.ciandt.summit.bootcamp2022.domain.data.entity.UserEntity;
+
+import java.util.Optional;
+
 public interface UserRepositoryPort {
+
+    String findUserTypeById(String userId);
+
+    Optional<UserEntity> findById(String userId);
 }
--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/infrastructure/adapter/repository/SpringUserRepository.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/infrastructure/adapter/repository/SpringUserRepository.java
@@ -2,9 +2,15 @@ package com.ciandt.summit.bootcamp2022.infrastructure.adapter.repository;
 
 import com.ciandt.summit.bootcamp2022.domain.data.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface SpringUserRepository extends JpaRepository<UserEntity, String> {
+    @Query("SELECT TipoUsuarioId from Usuarios WHERE Id = userId")
+    String findTypeById(String userId);
+    Optional<UserEntity> findById(String userId);
 }
 
