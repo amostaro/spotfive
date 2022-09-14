347b229c83fc227acabb7617712b99269d179d9c
Eric <ericandradedesousa8@hotmail.com>
2022-09-14 09:42:11 -0300

    [Feature] Correção try catch validantion

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/infrastructure/configuration/security/ValidationToken.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/infrastructure/configuration/security/ValidationToken.java
@@ -2,12 +2,14 @@ package com.ciandt.summit.bootcamp2022.infrastructure.configuration.security;
 
 import com.ciandt.summit.bootcamp2022.infrastructure.configuration.feign.AuthenticationApiIntegration;
 import com.ciandt.summit.bootcamp2022.infrastructure.configuration.security.dto.CreateAuthorizerRequest;
+import feign.FeignException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -27,8 +29,10 @@ public class ValidationToken {
             SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
 
             filterChain.doFilter(request, response);
-        } catch (Exception e) {
+        } catch (FeignException.Unauthorized e) {
             response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "The token is not valid.");
+        } catch (ServletException e) {
+            throw new RuntimeException(e);
         }
     }
 }
